a7a3f42c7dc2842575ad4a2de814623c21960d3b
ztzn <zaynutdinovazat@gmail.com>
2018-12-12 18:11:12 +0300

    add policy

---
+++ app/policies/feedback_policy.rb
@@ -0,0 +1,21 @@
+class FeedbackPolicy < ApplicationPolicy
+  def create?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def index?
+    true if user.present? && user.role == 'admin'
+  end
+
+  def update?
+    false
+  end
+
+  def destroy?
+    false
+  end
+end
